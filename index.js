ce09e8732e3fd46c7d788c5114494444124dbb97
Mwagiru <joymwagiru@gmail.com>
2022-03-27 11:26:14 +0300

    Almost done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+DocumentFragment.body.append(element);
+const header = document.createElement("h1");
+header.id="victory";
+header.innerHTML = "<h1 id="victory">Joy is the champion.</h1>"
