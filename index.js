63345ea0847ee88b1c7c5b88ed4f1c443143d588
Mwagiru <joymwagiru@gmail.com>
2022-04-01 17:18:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Write your code here!
-const main = document.querySelector("main");
+/*const main = document.querySelector("main");
 main.remove();
 DocumentFragment.body.append(element);
 const header = document.createElement("h1");
 header.id="victory";
-header.innerHTML = "<h1 id="victory">Joy is the champion.</h1>"
+header.innerHTML = "<h1>Joy is the champion.</h1>"
+*/
+const main = document.querySelector("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Joy is the champion.";
